acec176a1e82573987dc78719f8ed749a990c933
Rogerfernandobautista <atodomadres2000@gmail.com>
2023-06-23 10:25:42 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Card } from "./components/Card";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
